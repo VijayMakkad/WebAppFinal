7299c1eda41537cad19dd9a96644eabce4aa2e09
EasyGold-web <zlatotechnologies@gmail.com>
2024-05-27 01:37:52 +0530

    background (desktop)

---
+++ src/screens/Background/MainBg.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './MainBg.css';
+
+const MainBg = ({ children }) => {
+    return(
+        <div id="main">
+            <div className="backdrop">
+
+            </div>
+            <div className="boxes">
+
+                <div className="blue">
+
+                </div>
+                <div className="pink">
+
+                </div>
+                <div className="green">
+                    
+                </div>
+                <div className="black">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainBg;
