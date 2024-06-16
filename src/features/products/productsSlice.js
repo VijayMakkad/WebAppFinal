4d974269cd9a19d2b794c9f44f2c1648c0a1b77c
ktsrivastava29 <ktsrivastava29@gmail.com>
2024-06-16 22:41:47 +0530

    product api

---
+++ src/features/products/productsSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  products: [],
+  status: 'idle',
+  error: null,
+};
+
+export const fetchProductsByCategory = createAsyncThunk(
+  'products/fetchProductsByCategory',
+  async (category) => {
+//     const response = await axios.get(`http://127.0.0.1:8000/api/products/${category}`);
+
+const response = await axios.get('http://127.0.0.1:8000/api/products/popular');
+console.log(response);
+    return response.data.products;
+  }
+);
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProductsByCategory.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByCategory.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByCategory.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default productsSlice.reducer;
