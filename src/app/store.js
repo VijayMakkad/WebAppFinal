17b02fdb28d18889fdbac3cf4467c2ccf392557a
ktsrivastava29 <ktsrivastava29@gmail.com>
2024-06-20 18:29:51 +0530

    product api

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productsReducer from '../features/products/productsSlice';
-import authReducer from '../features/auth/authSlice'
+import cartReducer from '../features/cart/cartSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    products: productsReducer,
-    auth: authReducer
+    cart: cartReducer,
   },
 });
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import authstore from './features/auth/store';
+import store from '../src/app/store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
